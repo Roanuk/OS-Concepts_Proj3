b41fd6541c66c16f3db85745acac95a61fb48988
Douglas Cole Harrison <douglas.cole.harrison@gmail.com>
2016-11-30 19:24:02 -0600

    FCFS is implimented and code base is ready for rest of abstract inheriting schedulers.

--- Proj3.cpp
+++ Proj3.cpp
@@ -36,63 +36,90 @@ void printSchedulers()
 class Job
 {
 	public:
+		string name;
+		int arrivalT, durationT, executionT;
 		Job(string Name, int ArrivalT, int DurationT) : name(Name), arrivalT(ArrivalT), durationT(DurationT)
 		{
 			executionT = 0;
 		}
 		
 		//standard version of run continues until completion
-		void run()
+		string run()
 		{
-			run(durationT);
+			return run(durationT);
 		}
 		
 		//Overloaded version of run takes a number of slices
-		void run(int slices)
+		string run(int slices)
 		{
 			int runSlices = 0;
+			string returner(min(durationT,slices),' ');
 			while(executionT < durationT && runSlices < slices)
 			{
-				printf("%s ",name.c_str());
+				returner[runSlices] = name[0];
 				executionT++;
 				runSlices++;
 			}
+			return returner;
 		}
-		
-	protected:
-		string name;
-		int arrivalT, durationT, executionT;
 };
 
 class Scheduler 
 {
 	public:
-		Scheduler(SchedulerType schedulerType, vector<Job>* JobList)
+		Scheduler(vector<Job>* JobList): jobList(JobList)
 		{
-			jobList = JobList;
-			processJobs();
+			outputGraph.resize(jobList->size()+1); //make atleast as many rows in outputGraph as there are jobs + title row
 		}
+		
+		virtual void processJobs() = 0; //every scheduler must process job output into outputGraph
+
+		void reportOutput()
+		{
+			for(int outer = 0; outer < outputGraph.size();outer++)
+			{
+				for(int inner = 0; inner < outputGraph[outer].size(); inner++)
+				{
+					printf("%c",outputGraph[outer][inner]);
+				}
+				printf("\n");
+			}
+		}
+
+	protected:
+		vector<Job>* jobList; 
+		vector< vector<char> > outputGraph;
+};
+
+class scheduleFCFS : public Scheduler
+{
+	public:
+		scheduleFCFS(vector<Job>* JobList) : Scheduler(JobList) {}
+		
 		void processJobs()
 		{
-			int i;
-			for(i = 0; i<jobList->size(); i++)
-			{
-				(*jobList)[i].run();
-				printf("\n");
+			int inner, totalT = 0;
+			string buffer = "First Come First Serve Graph";
+			for(int outer = 0; outer < jobList->size()+1;outer++)
+			{				
+				outputGraph[outer].resize(buffer.size()+totalT,' ');
+				for(inner = 0; inner < buffer.size(); inner++)
+				{
+					outputGraph[outer][inner+totalT] = buffer[inner];
+				}								
+				buffer = (*jobList)[outer].run();
+				totalT += outer == 0 ? 0 : inner;
 			}
 
 		}
-	protected:
-		int sliceDur; //0 means nonpreemptive, >0 means # of slices per preemption
-		int numJobs; //number of jobs in jobList
-		vector<Job>* jobList; //
-		vector< vector<char> > outputGraph;
 };
 
 class OS
 {
 	public:
-		OS(ifstream* inputFile, SchedulerType schedulerType, int quantum)
+		OS(ifstream* InputFile, SchedulerType SchedulerType, int Quantum) : inputFile(InputFile), schedulerType(SchedulerType), quantum(Quantum) {}
+
+		Scheduler* executeScheduler() //returns Scheduler object
 		{
 			char* inBuffer = (char*)malloc(BUFFER_SIZE);
 			string name;
@@ -120,9 +147,38 @@ class OS
 				}
 			}
 			free(inBuffer);		
-			Scheduler instanceScheduler(schedulerType,&jobList);
+			switch(schedulerType)
+			{
+				case fcfs:
+						return new scheduleFCFS(&jobList);
+					break;
+
+				case rr:
+
+					break;
+
+				case spn:
+
+					break;
+
+				case srt:
+
+					break;
+
+				case hrrn:
+
+					break;
+
+				case fb:
+
+					break;
+			}
 		}
+
 	protected:
+		ifstream* inputFile;
+		SchedulerType schedulerType;
+		int quantum;
 		vector<Job> jobList;
 };
 
@@ -185,6 +241,10 @@ int main (int argc, char* argv[])
 	}
 
 	OS instanceOS(&inputFile,schedulerType,quantum);
+	Scheduler* instanceScheduler = instanceOS.executeScheduler();
+	instanceScheduler->processJobs();
+	instanceScheduler->reportOutput();
+	delete(instanceScheduler);
 	inputFile.close();
 	return 0;
 }
